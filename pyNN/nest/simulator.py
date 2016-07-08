ccadf1f100eb178a69d347ebd7d528f5af81adf2
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2016-07-08 17:54:34 +0200

    nest: Fix an error in min_delay calculation
    * Also: added 'node_uses_wfr' to the list of ignored attributes

--- pyNN/nest/simulator.py
+++ pyNN/nest/simulator.py
@@ -81,7 +81,8 @@ class _State(common.control.BaseState):
     def min_delay(self):
         # this rather complex implementation is needed to handle min_delay='auto'
         kernel_delay = nest.GetKernelStatus('min_delay')
-        syn_delay = nest.GetDefaults('static_synapse')['min_delay']
+        syn_delay = nest.GetDefaults('static_synapse').get(
+                'min_delay', numpy.inf)
         if syn_delay == numpy.inf or syn_delay > 1e300:
             return kernel_delay
         else:
